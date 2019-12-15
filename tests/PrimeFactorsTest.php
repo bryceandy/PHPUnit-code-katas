51cb440a5c9b61c416787bae4f928c23406676c2
bryceandy <bryceandy@rocketmail.com>
2019-12-15 09:35:09 +0300

    Declare return types

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -8,7 +8,7 @@ class PrimeFactorsTest extends TestCase
     /**
      * @test
      */
-    public function it_generates_prime_factors_for_1()
+    public function it_generates_prime_factors_for_1(): void
     {
         $factors = new PrimeFactors();
 

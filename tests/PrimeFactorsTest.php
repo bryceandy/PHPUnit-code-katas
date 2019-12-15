b2b7b1ff53f89ba32c3f6c71517e125dc0819f72
bryceandy <bryceandy@rocketmail.com>
2019-12-15 09:41:40 +0300

    Assert the first test case

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -14,4 +14,24 @@ class PrimeFactorsTest extends TestCase
 
         $this->assertEquals([1], $factors->generate(1));
     }
+
+    /**
+     * @test
+     */
+    public function it_generates_prime_factors_for_2(): void
+    {
+        $factors = new PrimeFactors();
+
+        $this->assertEquals([2], $factors->generate(2));
+    }
+
+    /**
+     * @test
+     */
+    public function it_generates_prime_factors_for_3(): void
+    {
+        $factors = new PrimeFactors();
+
+        $this->assertEquals([3], $factors->generate(3));
+    }
 }

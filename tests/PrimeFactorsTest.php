9ffb06c75c652af9984ec6381ceb87180424330b
bryceandy <bryceandy@rocketmail.com>
2019-12-15 09:31:28 +0300

    Assert the first test case

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\PrimeFactors;
 use PHPUnit\Framework\TestCase;
 
 class PrimeFactorsTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function it_generates_prime_factors_for_1()
+    {
+        $factors = new PrimeFactors();
 
+        $this->assertEquals([1], $factors->generate(1));
+    }
 }

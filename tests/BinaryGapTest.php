0ec5453b523967df92004a15be6f087908f55d8d
bryceandy <bryceandy@rocketmail.com>
2020-01-08 17:22:14 +0300

    Assert expected values from the data provider

---
+++ tests/BinaryGapTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\BinaryGap;
+use PHPUnit\Framework\TestCase;
+
+class BinaryGapTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checks
+     *
+     * @param $number
+     * @param $expected
+     */
+    public function it_counts_the_binary_gap_of_a_given_decimal($number, $expected): void
+    {
+        $binaryGap = new BinaryGap();
+        $this->assertEquals($expected, $binaryGap->findGap($number));
+    }
+
+    /**
+     * @return array
+     */
+    public function checks(): array
+    {
+        return [
+            [1041, 5],
+            [9, 2],
+            [15, 0],
+        ];
+    }
+}

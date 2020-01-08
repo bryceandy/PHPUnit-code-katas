0b9159842962eca1abb6c2ddd92e68e21cd300ee
bryceandy <bryceandy@rocketmail.com>
2020-01-08 17:14:11 +0300

    Create a binary gap algorithm

---
+++ src/BinaryGap.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+class BinaryGap
+{
+
+    public function findGap($N) {
+
+        $binNum = $this->createBinary($N);
+        $binArray = array_map('intval', str_split($binNum));
+        $binArrayLength = count($binArray);
+        $counter = 0;
+        $comparator = [];
+
+        for ($iterator = 1; $iterator < $binArrayLength; $iterator++ ) {
+            if ($binNum[$iterator] === 0) {
+                $counter++;
+            }
+
+            if ($binNum[$iterator] === 1 && $counter > 0){
+
+                $comparator[] = $counter;
+                $counter = 0;
+            }
+        }
+
+        return max($comparator);
+    }
+
+    public function createBinary($num, $binary = ''): int
+    {
+
+        while ($num % 2 === 0) {
+            $binary = '0'.$binary;
+            $num /= 2;
+        }
+
+        while ($num > 1 && $num % 2 === 1) {
+            $num--;
+            $binary = '1'.$binary;
+            $num /= 2;
+
+            while ($num % 2 === 0) {
+                $binary = '0'.$binary;
+                $num /= 2;
+            }
+        }
+
+        $binary = '1'.$binary;
+        return (int) $binary;
+    }
+}

1dd2bc3a60b4d1728342d0beaea8ce54b519b5fb
bryceandy <bryceandy@rocketmail.com>
2020-01-09 13:36:51 +0300

    Use the correct dataProvider as complete the algorithm

--- src/MissingPermutationElement.php
+++ src/MissingPermutationElement.php
@@ -7,6 +7,10 @@ class MissingPermutationElement
 
     public static function find($arr): int
     {
+        $numOfElements = count($arr);
+        $expectedSum = (($numOfElements + 1) * ($numOfElements + 2)) / 2;
+        $currentSum = array_sum($arr);
 
+        return $expectedSum - $currentSum;
     }
 }

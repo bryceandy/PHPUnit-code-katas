57312c5842b7a31bcea0c397d44667076e3e9ef5
bryceandy <bryceandy@rocketmail.com>
2020-01-13 16:25:40 +0300

    Create test for tape equilibrium

---
+++ tests/TapeEquilibriumTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\TapeEquilibrium;
+use PHPUnit\Framework\TestCase;
+
+class TapeEquilibriumTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checks
+     *
+     * @param $arr
+     * @param $expected
+     */
+    public function it_determines_the_minimum_difference_of_a_tape($arr, $expected): void
+    {
+        $this->assertEquals($expected, TapeEquilibrium::generate($arr));
+    }
+
+    /**
+     * @return array
+     */
+    private function checks(): array
+    {
+        return [
+            [[3, 1, 2, 4, 3], 1],
+        ];
+    }
+}

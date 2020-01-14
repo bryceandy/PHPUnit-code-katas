ab3fc292ee92264703e4bd3f7db56055f4ebdac9
bryceandy <bryceandy@rocketmail.com>
2020-01-14 13:43:00 +0300

    Create test for frog river jump

---
+++ tests/FrogRiverJumpTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\FrogRiverJump;
+
+class FrogRiverJumpTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @dataProvider checker
+     *
+     * @param $distance
+     * @param $positions
+     * @param $time
+     */
+    public function it_finds_the_minimal_time_the_frog_can_jump($distance, $positions, $time): void
+    {
+        $frogRiverJump = new FrogRiverJump();
+
+        $this->assertEquals($time, $frogRiverJump->estimate($distance, $positions));
+    }
+
+    /**
+     * @return array
+     */
+    public function checker(): array
+    {
+        return [
+            [[1, 3, 1, 4, 2, 3, 5, 4], 5, 6],
+        ];
+    }
+}

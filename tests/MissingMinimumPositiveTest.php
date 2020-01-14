0ea5640984b0820f3c03e99fd50e78d176bba1ef
bryceandy <bryceandy@rocketmail.com>
2020-01-14 18:37:17 +0300

    Re-word the function name

--- tests/MissingMinimumPositiveTest.php
+++ tests/MissingMinimumPositiveTest.php
@@ -13,7 +13,7 @@ class MissingMinimumPositiveTest extends TestCase
      * @param $arr
      * @param $expected
      */
-    public function it_determines_the_minimum_positive_missing_integer($arr, $expected): void
+    public function it_determines_the_missing_minimum_positive_integer($arr, $expected): void
     {
         $missingInteger = new MissingMinimumPositive();
 
@@ -29,6 +29,7 @@ class MissingMinimumPositiveTest extends TestCase
             [[1, 3, 6, 4, 1, 2], 5],
             [[1, 2, 3], 4],
             [[-1, -3], 1],
+            [[1789991], 1789992],
         ];
     }
 }

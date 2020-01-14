cd6883f874ced829e959e26ad00c393af19f4db3
bryceandy <bryceandy@rocketmail.com>
2020-01-14 14:39:58 +0300

    Create perm check algo

--- src/PermCheck.php
+++ src/PermCheck.php
@@ -4,5 +4,24 @@ namespace App;
 
 class PermCheck
 {
+    /**
+     * @param $A
+     *
+     * @return int
+     */
+    public function evaluate($A)
+    {
+        $trimmed_array = array_unique($A);
+        $largest_element = max($trimmed_array);
 
+        $current_sum = array_sum($trimmed_array);
+        $length = count($A);
+        $expected_sum = $length * ($length + 1) / 2;
+
+        if ($length === $largest_element && $current_sum === $expected_sum) {
+            return 1;
+        }
+
+        return 0;
+    }
 }

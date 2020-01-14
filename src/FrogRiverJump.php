a34d2c29a05b8bc386c0ee705ccad2707e31ead6
bryceandy <bryceandy@rocketmail.com>
2020-01-14 13:47:18 +0300

    Complete the frog river jump algorithm;

--- src/FrogRiverJump.php
+++ src/FrogRiverJump.php
@@ -4,5 +4,28 @@ namespace App;
 
 class FrogRiverJump
 {
+    /**
+     * @param $distance
+     * @param $positions
+     *
+     * @return int
+     */
+    public function estimate($distance, $positions): int
+    {
+        foreach ($positions as $key => $val) {
 
+            if (!isset($position_reached[$val])) {
+
+                $position_reached[$val] = true;
+
+                $distance--;
+            }
+
+            if (!$distance) {
+                return $key;
+            }
+        }
+
+        return -1;
+    }
 }

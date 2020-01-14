ef01297b7e15517d52f641cea5d7260d40b3ec5e
bryceandy <bryceandy@rocketmail.com>
2020-01-14 23:18:05 +0300

    Add a performant solution

--- src/MaxCounters.php
+++ src/MaxCounters.php
@@ -5,15 +5,56 @@ namespace App;
 class MaxCounters
 {
     /**
-     * Solution 1, 100% correctness 40% performance.
-     * Won't work well with very huge arrays. O(N*M)
+     * Solution 1, 100 % correctness & performance. O(N+M)
      *
      * @param $N
      * @param $A
      *
      * @return array
      */
-    public function operate($N, $A): array
+    public function operate($N, $A)
+    {
+        $counters = array_fill(0, $N, 0);
+        $maximumCounter = 0;
+        $currentMaximum = 0;
+
+        foreach ($A as $val) {
+
+            if ($val < $N + 1) {
+
+                if ($counters[$val - 1] < $maximumCounter) {
+
+                    $counters[$val - 1] = $maximumCounter;
+                }
+
+                $counters[$val - 1] ++;
+
+                if ($currentMaximum < $counters[$val - 1]) {
+
+                    $currentMaximum  = $counters[$val - 1];
+                }
+            }
+
+            else {
+
+                $maximumCounter = $currentMaximum;
+            }
+        }
+
+        for ($index = 0; $index < $N; $index++) {
+
+            if ($counters[$index] < $maximumCounter) {
+
+                $counters[$index] = $maximumCounter;
+            }
+        }
+
+        return $counters;
+    }
+
+    // Solution 2, 100% correctness 40% performance.
+     // Won't work well with very huge arrays. O(N*M)
+    public function operate2($N, $A)
     {
         $counters = array_fill(0, $N, 0);
         $countersLength = count($counters);
@@ -24,7 +65,7 @@ class MaxCounters
 
                 $counters[$val - 1] ++;
             }
-            else{
+            else {
                 $counters = array_fill(0, $N, max($counters));
             }
         }

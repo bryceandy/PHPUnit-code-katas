1decf8296d16fe16630bd32b4c1c0355d7a971bf
bryceandy <bryceandy@rocketmail.com>
2020-01-14 22:14:49 +0300

    Refactor algo and add comments

--- src/MaxCounters.php
+++ src/MaxCounters.php
@@ -5,7 +5,9 @@ namespace App;
 class MaxCounters
 {
     /**
-     * Solution 1, 100% correctness 40% performance
+     * Solution 1, 100% correctness 40% performance.
+     * Won't work well with very huge arrays. O(N*M)
+     *
      * @param $N
      * @param $A
      *
@@ -15,15 +17,14 @@ class MaxCounters
     {
         $counters = array_fill(0, $N, 0);
         $countersLength = count($counters);
-        $operationsLength = count($A);
 
-        for ($operInd = 0; $operInd < $operationsLength; $operInd++) {
+        foreach ($A as $val) {
 
-            if ($A[$operInd] < $countersLength + 1) {
+            if ($val < $countersLength + 1) {
 
-                $counters[$A[$operInd] - 1] ++;
+                $counters[$val - 1] ++;
             }
-            else {
+            else{
                 $counters = array_fill(0, $N, max($counters));
             }
         }

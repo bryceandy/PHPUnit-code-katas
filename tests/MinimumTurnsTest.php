4626f1629e4b2dc21be7de98b816bc1c870ee507
bryceandy <bryceandy@rocketmail.com>
2020-01-15 03:32:59 +0300

    Complete test for min turns

--- tests/MinimumTurnsTest.php
+++ tests/MinimumTurnsTest.php
@@ -23,7 +23,7 @@ class MinimumTurnsTest extends TestCase
     public function checks()
     {
         return [
-            [[1, 0, 1, 0, 1, 1], 2],
+            [[1, 0, 1, 0, 1, 1], 1],
             [[1, 1, 0, 1, 1], 2],
             [[0, 1, 0], 0],
             [[0, 1, 1, 0], 2],
